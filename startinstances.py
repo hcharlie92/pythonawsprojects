792c8c2166ea2568149178f24fbc0969020d1f47
Hindolo Charlie <dukehc92@gmail.com>
2023-01-10 18:59:49 -0500

    This is a python script that starts EC2 instances

---
+++ startinstances.py
@@ -0,0 +1,11 @@
+import boto3
+from time import sleep
+Ids=['i-02978a6140df0f82d','i-0a5b6f13f1ff04963', 'i-0bbf4bef8c22faef4']
+ec2 = boto3.resource('ec2')
+
+x = ec2.instances.filter(InstanceIds=Ids).start()
+
+#output
+print("starting instances")
+sleep(5)
+print(x)
